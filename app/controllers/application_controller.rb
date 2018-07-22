913bd378813631c7cdb14ec658c3c27bce4fb843
Jocelyn Chan <chan.jocelync@gmail.com>
2018-07-22 14:59:15 -0700

    Generate ability file, alias CRUD actions to :crud, add authenticate_user! global method to app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,17 @@ class ApplicationController < ActionController::Base
     end
   end
   helper_method(:current_user)
-  
-  
+
+  def user_signed_in?
+    current_user.present?
+  end
+  helper_method :user_signed_in?
+
+  def authenticate_user!
+    unless current_user.present?
+      flash[:danger] = "You must sign in or sign up first."
+      redirect_to new_session_path
+    end
+  end
+
 end
