faba95931b73e5f8e8c984139fbbf54f4a85e5de
Jocelyn Chan <chan.jocelync@gmail.com>
2018-07-21 18:18:25 -0700

    Add create and new action for users controller, add private method of user_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,29 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user  User.new user_params
+    if @user.save
+      flash[:success] = "Thank you #{@user.first_name} for signing up with Awesomo. Your account has been created."
+      redirect_to home_path
+    else 
+      flash[:danger] = "Your account was not created. Please try again."
+      render :new
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user)
+      .permit(
+      :first_name, 
+      :last_name, 
+      :email,
+      :password,
+      :password_confirmation
+    )
+  end
+
 end
