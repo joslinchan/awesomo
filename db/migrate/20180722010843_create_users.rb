356b9f8d1537f34c7580ffcabfc2ebe0e91b3d04
Jocelyn Chan <chan.jocelync@gmail.com>
2018-07-21 18:10:03 -0700

    Generate users table

---
+++ db/migrate/20180722010843_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email, index: {unique: true}
+      t.string :password_dirgest
+      t.boolean :admin, default: false
+
+      t.timestamps
+    end
+  end
+end
