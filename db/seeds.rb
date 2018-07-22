88eb0929384fecc847c8608bc7d945b3bb6cb623
Jocelyn Chan <chan.jocelync@gmail.com>
2018-07-22 14:46:05 -0700

    Set up seeds for users and super_user admin, not yet seeded though

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+PASSWORD = "supersecret"
+
+Comment.delete_all
+Post.delete_all
+User.delete_all
+
+super_user = User.create(
+  first_name: 'Jon',
+  last_name: 'Snow',
+  email: 'js@winterfell.gov',
+  password: PASSWORD,
+    admin: true
+)
+
+10.times do
+  first_name = Faker::Name.first_name
+  last_name = Faker::Name.last_name
+
+  User.create(
+    first_name: first_name,
+    last_name: last_name,
+    email: "#{first_name.downcase}.#{last_name.downcase}@example.com",
+    password: PASSWORD
+  )
+end
+
+users = User.all
+puts "👩👨 Created #{users.count} users"
