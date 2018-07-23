d4264b1a0b25a9f0446109d26ebbd97ab427e983
Jocelyn Chan <chan.jocelync@gmail.com>
2018-07-22 17:09:15 -0700

    Add edit action to users so that users are able to edit their first and last name + email. Add forms partial to users so that sign up and edit profile shares the same form

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
 
   get('/', {to: 'welcome#index', as: 'home'})
 
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :edit, :update]
+  get('/users/:id/edit_password', {to: 'users#edit_password', as: :edit_password})
+  patch('/users/:id/update_password', {to: 'users#update_password', as: :update_password})
 
   resource :session, only: [:new, :create, :destroy]
 
