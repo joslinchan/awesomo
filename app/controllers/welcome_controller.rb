b1486e8ec177a51a82563b04946e23c088af206a
Jocelyn Chan <chan.jocelync@gmail.com>
2018-08-02 17:52:56 -0700

    OMFG I CAN CALL ON COLOURLOVERS API via colour_retriever.rb, render json in welcome_controller, silently celebrates 😭🎏

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,9 @@
 class WelcomeController < ApplicationController
 
   def index
+    @palettes = ColourRetriever.new.parse_palettes
+    render json: @palettes
+    
   end
   
 end
---
+++ app/services/colour_retriever.rb
@@ -0,0 +1,13 @@
+class ColourRetriever
+  include HTTParty
+  base_uri "colourlovers.com/api"
+
+  def get_palettes
+    self.class.get("/palettes/?keywords=fish")
+  end
+
+  def parse_palettes
+    get_palettes.parsed_response
+  end
+
+end
