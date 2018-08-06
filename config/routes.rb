bb495eb83de2bc346c58c94ba577d2aca29cb5f9
Jocelyn Chan <chan.jocelync@gmail.com>
2018-08-05 17:31:26 -0700

    App back to not crashing- Previously decided to git rollback impulsively due to search_term table confusion... regrets...

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,14 @@ Rails.application.routes.draw do
 
   get('/', {to: 'welcome#index', as: 'home'})
 
-  resources :users, only: [:new, :create, :edit, :update]
+  resources :users, only: [:new, :create, :edit, :update] do
+    resources :search_terms, shallow: true, only: [:create]
+  end
   get('/users/:id/edit_password', {to: 'users#edit_password', as: :edit_password})
   patch('/users/:id/update_password', {to: 'users#update_password', as: :update_password})
 
   resource :session, only: [:new, :create, :destroy]
+
+  
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_22_023334) do
+ActiveRecord::Schema.define(version: 2018_08_05_224151) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "search_terms", force: :cascade do |t|
+    t.string "term"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "searchings", force: :cascade do |t|
+    t.bigint "search_term_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["search_term_id"], name: "index_searchings_on_search_term_id"
+    t.index ["user_id"], name: "index_searchings_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
@@ -26,4 +41,6 @@ ActiveRecord::Schema.define(version: 2018_07_22_023334) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "searchings", "search_terms"
+  add_foreign_key "searchings", "users"
 end
