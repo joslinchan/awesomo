24b143e163e8f45633f8187c82ef8c9097d4da27
Jocelyn Chan <chan.jocelync@gmail.com>
2018-08-06 13:17:30 -0700

    OH GOD IT SAVED- able to ColourPalette.create through link_to on welcome index. No search_term_id yet, but that's okay 👻

--- app/controllers/colour_palettes_controller.rb
+++ app/controllers/colour_palettes_controller.rb
@@ -4,14 +4,17 @@ class ColourPalettesController < ApplicationController
     paletteCollection = SearchApi.new.search_palettes params[:query]
     palette = paletteCollection.select{|key, value| key["id"] == params[:id]}   
 
-    saveTerm = SearchTerm.create(
+=begin     
+      saveTerm = SearchTerm.create(
       term: params[:query]
-      )
+      ) 
+=end
 
-    savePalette = ColourPalette.new(
+    savePalette = ColourPalette.create(
       title: palette.first["title"],
       url: palette.first["url"],
-      image_url: palette.first["imageUrl"]
+      image_url: palette.first["imageUrl"],
+      user: current_user
     )  
 
 
