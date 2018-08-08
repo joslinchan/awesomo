6ceea246545fbd6b3ceaaa5a05cab707a76526e4
Jocelyn Chan <chan.jocelync@gmail.com>
2018-08-08 11:04:13 -0700

    Attempt to connect to Oxford Dictionary API, but abandoning for now. 2 API's w/ 4 items are good enough for demo

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,12 +2,13 @@ class WelcomeController < ApplicationController
 
   def index
 
-    #@paletteCollection = SearchApi.new.search_palettes params[:query]
-    #@colourCollection = SearchApi.new.search_colours params[:query]
-    #@patternCollection = SearchApi.new.search_patterns params[:query]
+    @paletteCollection = SearchApi.new.search_palettes params[:query]
+    @colourCollection = SearchApi.new.search_colours params[:query]
+    @patternCollection = SearchApi.new.search_patterns params[:query]
 
     @photos = UnsplashRetriever.new.get_photos
-    #render json: @photos
+    #@data = DictionaryRetriever.new.get_data
+    #render json: @data
   end
   
 end
