4d2be2c05e814510b6bf0894dcb8be41c770c706
Jocelyn Chan <chan.jocelync@gmail.com>
2018-08-08 12:09:24 -0700

    Add empty inspirations controller, add create, destroy, index routes, delete unneeded routes

---
+++ app/controllers/inspirations_controller.rb
@@ -0,0 +1,3 @@
+class InspirationsController < ApplicationController
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,16 +3,8 @@ Rails.application.routes.draw do
 
   get('/', {to: 'welcome#index', as: 'home'})
 
-  resources :search_terms, only: [:create]
-
   resources :users, only: [:new, :create, :edit, :update] do
-    resources :colour_palettes, shallow: true, only: [:create, :destroy, :index]
-    resources :colours, shallow: true, only: [:create, :destroy, :index]
-    resources :patterns, shallow: true, only: [:create, :destroy, :index]
-  end
-  
-  resources :colour_palettes, only: [:create] do
-    resources :palette_likes, only: [:create]
+    resources :inspirations, shallow: true, only: [:create, :destroy, :index]
   end
 
   get('/users/:id/edit_password', {to: 'users#edit_password', as: :edit_password})
