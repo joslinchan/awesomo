d8d8c429d4d0de499a91ded5fc591efd0c923cf2
Jocelyn Chan <chan.jocelync@gmail.com>
2018-08-08 16:40:42 -0700

    Add validation to inspiration mode

--- app/models/inspiration.rb
+++ app/models/inspiration.rb
@@ -1,4 +1,7 @@
 class Inspiration < ApplicationRecord
   belongs_to :user
-  has_many :hexes
+  has_many :hexes, dependent: :destroy
+  
+  validates :image_url, :url, presence: true, uniqueness: { case_sensitive: false }
+
 end
