67fedbfb3fbe470b8ed8b43079a2a6caedbf4dcd
Jocelyn Chan <chan.jocelync@gmail.com>
2018-08-08 11:42:57 -0700

    Remove search_term, searching, & palette_like model

--- app/models/palette_like.rb
+++
@@ -1,6 +0,0 @@
-class PaletteLike < ApplicationRecord
-  belongs_to :user
-  belongs_to :colour_palette
-
-  validates :colour_palette_id, uniqueness: {scope: :user_id}
-end
--- app/models/searching.rb
+++
@@ -1,6 +0,0 @@
-class Searching < ApplicationRecord
-  belongs_to :search_term
-  belongs_to :user
-
-  validates :user_id, uniqueness: {scope: :user_id}
-end
