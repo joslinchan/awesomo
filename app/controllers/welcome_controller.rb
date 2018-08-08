9ad849c921e826c52bf16e849b97c7ea0e3c375e
Jocelyn Chan <chan.jocelync@gmail.com>
2018-08-08 11:11:05 -0700

    Remove search_term controller and empty view folder

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,14 +1,10 @@
 class WelcomeController < ApplicationController
 
   def index
-
     @paletteCollection = SearchApi.new.search_palettes params[:query]
     @colourCollection = SearchApi.new.search_colours params[:query]
     @patternCollection = SearchApi.new.search_patterns params[:query]
-
     @photos = UnsplashRetriever.new.get_photos
-    #@data = DictionaryRetriever.new.get_data
-    #render json: @data
   end
   
 end
