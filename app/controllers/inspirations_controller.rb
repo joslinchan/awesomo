5679035027dd5a52a3f5d253b35aa13cab8756f4
Jocelyn Chan <chan.jocelync@gmail.com>
2018-08-08 19:19:46 -0700

    Add inspiration controller, add #create, #destroy, #index. Add index view. Able to save all 4 types 🔥 Thank you Tim!

--- app/controllers/inspirations_controller.rb
+++ app/controllers/inspirations_controller.rb
@@ -1,3 +1,29 @@
 class InspirationsController < ApplicationController
-  
+  def create
+    inspiration = Inspiration.new(
+      title: params[:title],
+      url: params[:url],
+      image_url: params[:image_url],
+      user: current_user
+    )
+
+    if inspiration.save
+      flash[:success] = "Inspiration has been saved"
+      redirect_to home_path
+    else
+      flash[:danger] = "Inspiration already saved"
+      redirect_to home_path
+    end
+  end
+
+  def destroy
+    inspiration = Inspiration.find params[:id]
+    inspiration.destroy
+    redirect_to user_inspirations_path(current_user)
+  end
+
+  def index
+    @inspireCollection = current_user.inspirations
+  end
+
 end
