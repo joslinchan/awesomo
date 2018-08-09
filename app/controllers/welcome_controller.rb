9ec587d95bbb78b6b73b74623422c44c87617c76
Jocelyn Chan <chan.jocelync@gmail.com>
2018-08-08 20:50:29 -0700

    HOLY MOLY- combine everything into one giant array and shuffle it. Able to display both colourlovers and unsplash items on welcome index 🔮

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,10 +1,13 @@
 class WelcomeController < ApplicationController
 
   def index
-    @paletteCollection = SearchApi.new.search_palettes params[:query]
-    @colourCollection = SearchApi.new.search_colours params[:query]
-    @patternCollection = SearchApi.new.search_patterns params[:query]
-    @photos = UnsplashRetriever.new.get_photos
+    paletteCollection = SearchApi.new.search_palettes params[:query]
+    colourCollection = SearchApi.new.search_colours params[:query]
+    patternCollection = SearchApi.new.search_patterns params[:query]
+    photos = UnsplashRetriever.new.get_photos
+
+    @everything = paletteCollection + colourCollection + patternCollection + photos
+
   end
   
 end
