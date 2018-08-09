87dd1dbf6412d544f0755617dad9ded187504ed3
Jocelyn Chan <chan.jocelync@gmail.com>
2018-08-09 12:30:09 -0700

    Limit api/v1/inspiration route actions to only index, show, create, & destroy

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: {format: :json} do
     namespace :v1 do
-      resources :inspirations
+      resources :inspirations, only: [:index, :show, :create, :destroy]
     end
   end
 
