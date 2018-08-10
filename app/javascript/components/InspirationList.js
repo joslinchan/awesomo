44570b2acb843125607c4b74895cda6bbdbe0b09
Jocelyn Chan <chan.jocelync@gmail.com>
2018-08-10 16:19:23 -0700

    InspirationList is rendering all the details properly 👍

--- app/javascript/components/InspirationList.js
+++ app/javascript/components/InspirationList.js
@@ -4,14 +4,31 @@ class InspirationList extends Component {
   constructor(props) {
     super(props);
 
-    console.log(props);
+    this.state = {
+      inspirations: [...props.inspirations]
+    };
+
+    console.log(this.state);
   }
   
   render() {
+    const {inspirations} = this.state;
+
     return(
       <div className="InspirationList">
         <h1>Inspirations</h1>
         <ul>
+          {inspirations.map((inspire, index) => (
+            <li key = {inspire.id}>
+              <a href={inspire.url}><p>{inspire.title}</p></a>
+              <a href={inspire.url}><img src={inspire.image_url} /></a>
+              {inspire.hexes.map(hex => (
+                <ul>
+                  <li>{hex.code}</li>
+                </ul>
+              ))}
+            </li>
+          ))}
         </ul>
       </div>
     );
