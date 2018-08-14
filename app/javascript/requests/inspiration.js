ac6a97f5a344dc3e5098d44f3071ff3e1522a58d
Jocelyn Chan <chan.jocelync@gmail.com>
2018-08-13 17:40:45 -0700

    Add React api requests: requests/config, inspiration, session, user

---
+++ app/javascript/requests/inspiration.js
@@ -0,0 +1,35 @@
+import {BASE_URL} from "./config";
+
+const Inspiration = {
+  all() {
+   return fetch(`${BASE_URL}`, {
+     credentials: "include"
+   })
+   .then(
+     res => res.json()
+   );
+  },
+  one(id) {
+    return fetch(`${BASE_URL}/inspirations/${id}`, {
+      credentials: "include"
+    })
+    .then (
+      res => res.json()
+    );
+  },
+  create(params) {
+    return fetch(`${BASE_URL}/inspirations`, {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(params)
+    })
+    .then (
+      res => re.json()
+    );
+  }
+};
+
+export default Inspiration;
