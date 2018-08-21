11886a008866af950842ac5cec0ef28ffa6f8e67
Jocelyn Chan <chan.jocelync@gmail.com>
2018-08-21 10:27:59 -0700

    Delete unused single item Api call in search_api.rb. Still need to rename that file to something more suitable

--- app/services/search_api.rb
+++ app/services/search_api.rb
@@ -4,36 +4,15 @@ class SearchApi
     palettes = ColourRetriever.new.get_palettes(search_params)
     paletteCollection = palettes.try(:[],"palettes").try(:[], "palette")
   end
- 
-=begin   
-    def search_single_palette(id)
-    palette = ColourRetriever.new.get_palette(id)
-    parsedPalette = palette["palettes"]["palette"]
-  end  
-=end
 
   def self.parse_colours(search_params)
     colours = ColourRetriever.new.get_colours(search_params)
     colourCollection = colours.try(:[], "colors").try(:[], "color")
   end
 
-=begin 
-  def search_single_colour(id)
-    colour = ColourRetriever.new.get_colour(id)
-    parsedColour = colour["colors"]["color"]
-  end
-=end
-
   def self.parse_patterns(search_params)
     patterns = ColourRetriever.new.get_patterns(search_params)
     patternCollection = patterns.try(:[],"patterns").try(:[], "pattern")
   end 
-
-=begin 
-  def search_single_pattern(id)
-    pattern = ColourRetriever.new.get_pattern(id)
-    parsedPattern = pattern["patterns"]["pattern"]
-  end 
-=end
 
 end
