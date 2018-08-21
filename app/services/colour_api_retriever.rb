a7b14fbabb7f43e4f1fd90899413fd28db8416c5
Jocelyn Chan <chan.jocelync@gmail.com>
2018-08-21 10:35:01 -0700

    Remove unused single item third party api calls in ColourApiRetriever

--- app/services/colour_api_retriever.rb
+++ app/services/colour_api_retriever.rb
@@ -5,25 +5,13 @@ class ColourApiRetriever
   def get_palettes(search_term)
     self.class.get("/palettes/?keywords=#{search_term}")
   end 
-  
-  def get_palette(id)
-    self.class.get("/palette/#{id}")
-  end  
 
   def get_colours(search_term)
     self.class.get("/colors/?keywords=#{search_term}")
   end
-  
-  def get_colour(id)
-    self.class.get("/color/#{id}")
-  end
 
   def get_patterns(search_term)
     self.class.get("/patterns/?keywords=#{search_term}")
-  end
-
-  def get_pattern(id)
-    self.class.get("/pattern/#{id}")
   end
 
 end
