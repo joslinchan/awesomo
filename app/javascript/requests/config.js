93a7243d179dd594cfd0e9d6d17aab14a43e5a1d
Jocelyn Chan <chan.jocelync@gmail.com>
2018-08-31 17:41:10 -0700

    Refix config.js

--- app/javascript/requests/config.js
+++ app/javascript/requests/config.js
@@ -1,4 +1,3 @@
 export const HOST = "awesomo-search.herokuapp.com";
-export const BASE_URL = `https://${HOST}`;
-/* export const API_BASE = "/api/v1"; */
-/* export const BASE_URL = `https://${HOST}${API_BASE}`; */
+export const API_BASE = "/api/v1";
+export const BASE_URL = `https://${HOST}${API_BASE}`;
