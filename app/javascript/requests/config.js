5a56d1d0f481553dc6cd8d7473b17b385d555a07
Jocelyn Chan <chan.jocelync@gmail.com>
2018-08-31 17:33:38 -0700

    fix extra backslash in user.js

--- app/javascript/requests/config.js
+++ app/javascript/requests/config.js
@@ -1,4 +1,4 @@
-export const HOST = "awesomo-search.herokuapp.com/";
+export const HOST = "awesomo-search.herokuapp.com";
 export const BASE_URL = `https://${HOST}`;
 /* export const API_BASE = "/api/v1"; */
 /* export const BASE_URL = `https://${HOST}${API_BASE}`; */
