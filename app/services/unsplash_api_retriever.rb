d1f47126f8dd4988d4c25554684c2c37371a4f19
Jocelyn Chan <chan.jocelync@gmail.com>
2018-09-04 19:31:18 -0700

    Trying to config with Heroku again

--- app/services/unsplash_api_retriever.rb
+++ app/services/unsplash_api_retriever.rb
@@ -2,8 +2,8 @@ class UnsplashApiRetriever
 
   def initialize
     Unsplash.configure do |config|
-      config.application_access_key = ENV[:unsplash_access_key] ||Rails.application.credentials[:unsplash_access_key]
-      config.application_secret = ENV[:unsplash_secret_key] ||Rails.application.credentials[:unsplash_secret_key]
+      config.application_access_key = unsplash_access_key ||Rails.application.credentials[:unsplash_access_key]
+      config.application_secret = unsplash_secret_key ||Rails.application.credentials[:unsplash_secret_key]
       config.utm_source = "awesomo_client_app"
     end
   end
