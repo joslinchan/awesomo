6443abfbe8ccb19ac3e25ba6a553d9064c82b1df
Jocelyn Chan <chan.jocelync@gmail.com>
2018-09-08 21:33:56 -0700

    Resolve merge conflict for redirect in SignInPage

--- app/javascript/components/SignInPage.jsx
+++ app/javascript/components/SignInPage.jsx
@@ -31,12 +31,8 @@ class SignInPage extends Component {
         const {onSignIn = () => {}} = this.props;
         onSignIn();
         /* console.log(this.props.history); */
-<<<<<<< HEAD
-        window.location.replace('/inspirations/search');
-=======
         /* this.props.history.push("/"); */
         window.location.replace('/inspiration/search');
->>>>>>> master
       }
     });
   }
