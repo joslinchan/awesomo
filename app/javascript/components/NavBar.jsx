0de75d7e36405408c8fb10cd5a21c951a8ad7936
Jocelyn Chan <chan.jocelync@gmail.com>
2018-09-24 18:06:50 -0700

    Fix spacing in NavBar

--- app/javascript/components/NavBar.jsx
+++ app/javascript/components/NavBar.jsx
@@ -15,67 +15,70 @@ const NavBar = props => {
         <span className="icon">
         </span>
       </a>
-    <section className="NavBar mb-2">
-
-      {currentUser ? (
-        <React.Fragment>
-          <span className="text-light lead">Hello {currentUser.first_name}!</span>
-          <NavLink 
-            className="btn btn-dark" 
-            exact 
-            to="/">
-            Intro
-          </NavLink>
-          <NavLink 
-            className="btn btn-dark"
-            exact 
-            to="/inspirations/search"
-          >
-            Search
-          </NavLink>
-          <NavLink 
-            className="btn btn-dark"
-            exact 
-            to="/inspirations"
-          >
-            Collection
-          </NavLink>
-          <a 
-            className="btn btn-dark"
-            onClick={handleClick} 
-            href="#"
-          >
-            Sign Out 
-            <i className="fas fa-sign-out-alt"></i>
-          </a>
-        </React.Fragment>
-      ) : (
-        <span className="sign">
-        <NavLink 
-          className="btn btn-dark" 
-          exact 
-          to="/"
-        >
-          Intro
-        </NavLink>
-        <NavLink 
-          className="btn btn-dark"
-          exact 
-          to="/sign_up"
-        >
-          Sign Up
-        </NavLink>
-        <NavLink 
-          className="btn btn-dark"
-          exact 
-          to="/sign_in"
-        >
-          Sign In 
-          <i className="fas fa-sign-in-alt"></i>
-        </NavLink>
-        </span>
-      )}
-    </section>
+      
+      <section className="NavBar mb-2">
+        {currentUser ? (
+          <React.Fragment>
+            <span className="text-light lead">
+              Hello {currentUser.first_name}!
+            </span>
+            <NavLink 
+              className="btn btn-dark" 
+              exact 
+              to="/"
+            >
+              Intro
+            </NavLink>
+            <NavLink 
+              className="btn btn-dark"
+              exact 
+              to="/inspirations/search"
+            >
+              Search
+            </NavLink>
+            <NavLink 
+              className="btn btn-dark"
+              exact 
+              to="/inspirations"
+            >
+              Collection
+            </NavLink>
+            <a 
+              className="btn btn-dark"
+              onClick={handleClick} 
+              href="#"
+            >
+              Sign Out 
+              <i className="fas fa-sign-out-alt"></i>
+            </a>
+          </React.Fragment>
+        ) : (
+          <span className="sign">
+            <NavLink 
+              className="btn btn-dark" 
+              exact 
+              to="/"
+            >
+              Intro
+            </NavLink>
+            <NavLink 
+              className="btn btn-dark"
+              exact 
+              to="/sign_up"
+            >
+              Sign Up
+            </NavLink>
+            <NavLink 
+              className="btn btn-dark"
+              exact 
+              to="/sign_in"
+            >
+              Sign In 
+              <i className="fas fa-sign-in-alt"></i>
+            </NavLink>
+          </span>
+        )}
+      </section>
     </nav>
   )
 }
