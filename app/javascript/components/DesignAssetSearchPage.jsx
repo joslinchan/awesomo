e0cde5967eb217e988f48a24d4c3369e92c4dfba
Jocelyn Chan <chan.jocelync@gmail.com>
2018-09-29 23:49:40 -0700

    Apply ESLint to DesignAssetSearchPage

--- app/javascript/components/DesignAssetSearchPage.jsx
+++ app/javascript/components/DesignAssetSearchPage.jsx
@@ -1,8 +1,8 @@
-import React, { Component } from "react";
-import InspirationRailsApi from "../requests/inspiration";
-import DesignAssetSearchDetails from "./DesignAssetSearchDetails";
-import Tippy from "./ReactTippy";
-import shuffle from "lodash/shuffle";
+import React, { Component } from 'react';
+import shuffle from 'lodash/shuffle';
+import InspirationRailsApi from '../requests/inspiration';
+import DesignAssetSearchDetails from './DesignAssetSearchDetails';
+import Tippy from './ReactTippy';
 
 class DesignAssetSearchPage extends Component {
   constructor(props) {
@@ -10,10 +10,10 @@ class DesignAssetSearchPage extends Component {
 
     this.state = {
       loading: false,
-      fetched_design_assets: [],
-      term: "",
-      errorMessage: "",
-    }
+      fetchedDesignAssets: [],
+      term: '',
+      errorMessage: '',
+    };
   }
 
   onInputChange = (term) => {
@@ -22,55 +22,54 @@ class DesignAssetSearchPage extends Component {
 
   enterSubmit = (event) => {
     event.preventDefault();
-    const { currentTarget } = event;
-    const { term } = this.state;  
+    /* const { currentTarget } = event; */
+    const { term } = this.state;
 
-    this.setState({ 
-      fetched_design_assets: [], 
-      errorMessage: "", 
-      loading: true 
-    })
+    this.setState({
+      fetchedDesignAssets: [],
+      errorMessage: '',
+      loading: true,
+    });
 
     InspirationRailsApi.search(term)
-    .then(fetched_design_assets => {
-      if (fetched_design_assets.status === 404) {
+      .then((fetchedDesignAssets) => {
+        if (fetchedDesignAssets.status === 404) {
+          return this.setState({
+            errorMessage: 'No results found',
+            loading: false,
+          });
+        }
+
+        const shuffledDesignAssets = shuffle(Array.from(fetchedDesignAssets));
         return this.setState({
-          errorMessage: "No results found",
+          fetchedDesignAssets: shuffledDesignAssets,
           loading: false,
         });
-      };
-
-      const shuffledDesignAssets = shuffle(Array.from(fetched_design_assets));
-      return this.setState({ 
-        fetched_design_assets: shuffledDesignAssets,
-        loading: false,
       });
-    });
   };
 
   render() {
-    const { errorMessage, loading, fetched_design_assets } = this.state;
+    const { errorMessage, loading, fetchedDesignAssets } = this.state;
 
-    return(
+    return (
       <main className="container">
 
-        <div className="logo">
-        </div>
+        <div className="logo" />
 
         <form onSubmit={this.enterSubmit}>
           <div>
-            <Tippy 
-              duration={200} 
-              delay={50} 
-              arrow={true} 
-              arrowType="round" 
+            <Tippy
+              duration={200}
+              delay={50}
+              arrow
+              arrowType="round"
               animation="scale"
             >
-              <input 
+              <input
                 className="form-control underline search"
-                placeholder="Search for..." 
-                name="query" 
-                id="query" 
+                placeholder="Search for..."
+                name="query"
+                id="query"
                 type="text"
                 title="Enter a search term!"
                 onChange={event => this.onInputChange(event.target.value)}
@@ -79,32 +78,32 @@ class DesignAssetSearchPage extends Component {
           </div>
 
           <div className="search-buttons">
-            <Tippy 
-              duration={200} 
-              delay={50} 
-              arrow={true} 
-              arrowType="round" 
+            <Tippy
+              duration={200}
+              delay={50}
+              arrow
+              arrowType="round"
               animation="scale"
             >
-              <input 
+              <input
                 className="btn btn-outline-dark search icon"
-                type="submit" 
-                value="&#xf002;" 
+                type="submit"
+                value="&#xf002;"
                 title="✨Search!"
               />
             </Tippy>
-            
-            <Tippy 
-              duration={200} 
-              delay={50} 
-              arrow={true} 
-              arrowType="round" 
+
+            <Tippy
+              duration={200}
+              delay={50}
+              arrow
+              arrowType="round"
               animation="scale"
             >
-              <input 
+              <input
                 className="btn btn-outline-dark lucky search"
-                type="submit" 
-                value="I'm Feeling Lucky" 
+                type="submit"
+                value="I'm Feeling Lucky"
                 title="Find me something random!"
               />
             </Tippy>
@@ -117,18 +116,18 @@ class DesignAssetSearchPage extends Component {
             <h3 className="load">Loading...</h3>
           ) : (
             <div id="box">
-              {fetched_design_assets.map((design_asset, index) => (
-                <div key={index}>
-                  <DesignAssetSearchDetails {...design_asset} />
+              {fetchedDesignAssets.map(designAsset => (
+                <div key={designAsset.url}>
+                  <DesignAssetSearchDetails {...designAsset} />
                 </div>
               ))}
-            </div> 
+            </div>
           )}
         </section>
 
       </main>
     );
-  };
-};
+  }
+}
 
 export default DesignAssetSearchPage;
