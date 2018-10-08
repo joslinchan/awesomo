8f524b28e0c2130beb12fe18a454d9b19d26bca5
Jocelyn Chan <chan.jocelync@gmail.com>
2018-10-07 21:48:59 -0700

    Apply EsLint to WeclomePage

--- app/javascript/components/SearchBar.jsx
+++ app/javascript/components/SearchBar.jsx
@@ -1,50 +1,49 @@
-import React, {Component} from "react";
-import InspirationApi from "../requests/inspiration";
+import React, { Component } from 'react';
+import InspirationApi from '../requests/inspiration';
 
 class SearchBar extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      term: undefined
-    }
+      term: undefined,
+    };
 
     this.onInputChange = this.onInputChange.bind(this);
     this.enterSubmit = this.enterSubmit.bind(this);
   }
 
   onInputChange(term) {
-    this.setState({term});
+    this.setState({ term });
     this.props.onTermChange(term);
   }
 
   handleInputChange(term) {
-    this.setState({term});
+    this.setState({ term });
     this.props.onTermChange(term);
   }
 
   enterSubmit(event) {
     event.preventDefault();
-    const {currentTarget} = event; //const currentTarget = event.currentTarget
-    //console.log(currentTarget);
-    const { term } = this.state;   // const term = this.state.term;
+    const { currentTarget } = event; // const currentTarget = event.currentTarget
+    // console.log(currentTarget);
+    const { term } = this.state; // const term = this.state.term;
 
     InspirationApi.search(term)
-    .then(searches => {
-      this.setState({searches: searches});
-    })
+      .then((searches) => {
+        this.setState({ searches });
+      });
   }
 
   render() {
-
-    return(
+    return (
       <main>
         <form onSubmit={this.enterSubmit}>
           <div>
-            <input 
-              placeholder="Search for..." 
-              name="query" 
-              id="query" 
+            <input
+              placeholder="Search for..."
+              name="query"
+              id="query"
               type="text"
               onChange={event => this.onInputChange(event.target.value)}
             />
@@ -56,8 +55,8 @@ class SearchBar extends Component {
         </form>
 
       </main>
-    )
+    );
   }
-};
+}
 
 export default SearchBar;
--- app/javascript/components/WelcomePage.jsx
+++ app/javascript/components/WelcomePage.jsx
@@ -1,83 +1,86 @@
-import React, {Component} from "react";
+import React from 'react';
 
-class WelcomePage extends Component {
+const WelcomePage = {
   render() {
-    return(
+    return (
       <main>
         <a href="/inspirations/search">
-          <div className="logo">
-          </div>
+          <div className="logo" />
         </a>
         <hr />
         <h2>A mini search engine for your inspirational needs</h2>
-        <div className="p">Finally, you don't have to waste time looking for design assets.<br /> Just type in a search term and have colour swatches, palettes, patterns, and high resolution photos delivered to you instantly.</div>
+        <div className="p">
+          Finally, you don&apos;t have to waste time looking for design assets.
+          <br />
+          {' '}
+          Just type in a search term and have colour swatches, palettes, patterns, and high resolution photos delivered to you instantly.
+        </div>
         <hr />
 
         <div className="container welcome">
- 
+
           <h1>
             <div className="circle">
-            <i className="fas fa-lemon welcome"></i>
+              <i className="fas fa-lemon welcome" />
             </div>
           </h1>
           <h4>Easy peasy, lemon-squeezy</h4>
           <p className="welcome">
-            Working on an app and need some cat images? Just type in "cat" in the search bar. Pick a colour scheme for your project while you're at it. This is easier than making lemonade.
+            Working on an app and need some cat images? Just type in &ldquo;cat&rdquo; in the search bar. Pick a colour scheme for your project while you&apos;re at it. This is easier than making lemonade.
           </p>
 
           <h1>
             <div className="circle">
-            <i className="fas fa-search welcome"></i>
+              <i className="fas fa-search welcome" />
             </div>
           </h1>
           <h4>Search by keyword</h4>
           <p className="welcome">
-            Yes, it's that simple. No more countless hours of perusing through endless google searches. There are just too many options and distractions out there. You just want to get back to coding. One keyword, one search- Done!
+            Yes, it&apos;s that simple. No more countless hours of perusing through endless google searches. There are just too many options and distractions out there. You just want to get back to coding. One keyword, one search- Done!
           </p>
 
           <h1>
             <div className="circle">
-            <i className="fas fa-cloud-upload-alt welcome"></i>
+              <i className="fas fa-cloud-upload-alt welcome" />
             </div>
           </h1>
           <h4>Save attribute</h4>
           <p className="welcome">
-            Alien invasion happening while you're in the middle of a search? Don't fret! You can save as many items as you like for later viewing. Even if your computer gets destroyed, your items are stored safely in the cloud.
+            Alien invasion happening while you&apos;re in the middle of a search? Don&apos;t fret! You can save as many items as you like for later viewing. Even if your computer gets destroyed, your items are stored safely in the cloud.
           </p>
 
           <h1>
             <div className="circle">
-            <i className="fas fa-undo-alt welcome"></i>
+              <i className="fas fa-undo-alt welcome" />
             </div>
           </h1>
           <h4>Unsave feature</h4>
           <p className="welcome">
-            Changed your mind after saving? We feel ya'! You can unsave a saved item any time. No commitments necessary.
+            Changed your mind after saving? We feel ya&apos;! You can unsave a saved item any time. No commitments necessary.
           </p>
 
           <h1>
             <div className="circle">
-            <i className="fas fa-trash welcome"></i>
+              <i className="fas fa-trash welcome" />
             </div>
           </h1>
           <h4>Delete ability</h4>
           <p className="welcome">
-            The power is in your hands. Don't like an item in your saved collection? Go ahead and delete it. You're the boss.
+            The power is in your hands. Don&apos;t like an item in your saved collection? Go ahead and delete it. You&apos;re the boss.
           </p>
 
           <h1>
             <div className="circle">
-            <i className="fas fa-glasses welcome"></i>
+              <i className="fas fa-glasses welcome" />
             </div>
           </h1>
           <h4>Sneak peak inside</h4>
-          <div className="demo">
-          </div>
-      
+          <div className="demo" />
+
         </div>
       </main>
-    )
-  }
-}
+    );
+  },
+};
 
 export default WelcomePage;
