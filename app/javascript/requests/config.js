db1bf39688ce95a0b26d828680ccb79ad7eea7be
Jocelyn Chan <chan.jocelync@gmail.com>
2018-10-13 20:02:41 -0700

    Apply ESLint to config.js

--- app/javascript/requests/config.js
+++ app/javascript/requests/config.js
@@ -1,3 +1,3 @@
-export const HOST = "localhost:4000";
-export const API_BASE = "/api/v1";
+export const HOST = 'localhost:4000';
+export const API_BASE = '/api/v1';
 export const BASE_URL = `http://${HOST}${API_BASE}`;
